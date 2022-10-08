ab4dad867355067d26f4ee6c9fa63322e14b0529
alwaysraihan <bdyoungdeveloper@gmail.com>
2022-10-08 20:57:41 +0600

    build: intial setup completed

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Splash from "./screen/Splash";
-import MainScreen from "./screen/MainScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import MainScreen from './screen/MainScreen';
+import Splash from './screen/Splash';
 
 const Stack = createStackNavigator();
 
@@ -10,16 +10,8 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Splash}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MainScreen"
-          component={MainScreen}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" component={Splash} options={{ headerShown: false }} />
+        <Stack.Screen name="MainScreen" component={MainScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
